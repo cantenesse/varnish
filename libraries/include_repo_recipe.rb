d0ef50f69cee3c74c7a564ddd3cb2694007e7f60
Chris Antenesse <chris@foundapp.com>
2014-01-22 22:14:47 -0800

    changed url for apt repo

--- libraries/include_repo_recipe.rb
+++ libraries/include_repo_recipe.rb
@@ -3,7 +3,7 @@ class Chef::Recipe
     case platform
 	when "debian"
       apt_repository "varnish-cache" do
-		uri "http://repo.varnish-cache.org/#{node['platform']}"
+		uri "http://repo.varnish-cache.org/dists/#{node['platform']}"
 		distribution node['lsb']['codename']
 		components ["varnish-#{node['varnish']['version']}"]
 		key "http://repo.varnish-cache.org/#{node['platform']}/GPG-key.txt"
