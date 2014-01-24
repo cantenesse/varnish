a59bde32b5bf6e317b9e62e6bf17fde77902caa8
Chris Antenesse <chris@foundapp.com>
2014-01-23 20:07:45 -0800

    fixed string formatting for url parameter in repo provider

--- libraries/include_repo_recipe.rb
+++ libraries/include_repo_recipe.rb
@@ -3,7 +3,7 @@ class Chef::Recipe
     case platform
 	when "debian"
       apt_repository "varnish-cache" do
-		uri "http://repo.varnish-cache.org/dists/#{node['platform']}"
+		uri "http://repo.varnish-cache.org/#{node['platform']}/dists/#{node['lsb']['codename']}"
 		distribution node['lsb']['codename']
 		components ["varnish-#{node['varnish']['version']}"]
 		key "http://repo.varnish-cache.org/#{node['platform']}/GPG-key.txt"
@@ -11,7 +11,7 @@ class Chef::Recipe
 	when "rhel"
 	  major_version = node[:platform_version].split('.')[0]
       yum_repository 'varnish-cache' do
-        url "http://repo.varnish-cache.org/redhat/varnish-3.0/el#{node['major_version']}/$basearch"
+        url "http://repo.varnish-cache.org/redhat/varnish-3.0/el#{major_version}/$basearch"
         gpgcheck false
         action :create
 	  end
